6d9e0a9e216280a4907adf094e655330888254d7
François-Xavier Ponscarme <fx.ponscarme.pro@gmail.com>
2017-12-31 16:00:03 +0100

    feat: add photonkit and bulma styles

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import 'photonkit/dist/css/photon.css'
+import 'bulma/css/bulma.css'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
